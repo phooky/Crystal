f48a027625e5b540a02013be3ee4267bb3d51ef4
Adam Mayer <me@phooky.name>
2013-07-06 15:56:10 -0400

    added screen mapping method

--- generate.py
+++ generate.py
@@ -2,7 +2,15 @@ import pattern
 import ply
 import projection
 
-p=pattern.make_depths(800,600,10,1000)
+res = (800,600)
+# screen upper-left phy coords, in.
+s1 = (-0.2,-0.15)
+# screen lower-righ phy coords, in.
+s2 = (0.2, 0.15)
+# screen distance
+p=pattern.make_depths(res[0],res[1],1.0,3.0)
 ply.write_ply('out.ply',p)
-p=projection.project_points(p,0.4,0.0001,0.0001,800,600)
-ply.write_ply('outp.ply',p)
+p=projection.map_to_screen(p,(0,0),res,s1,s2)
+ply.write_ply('outm.ply',p)
+#p=
+#ply.write_ply('outp.ply',p)
--- projection.py
+++ projection.py
@@ -8,6 +8,19 @@ def project(point,ds,xscale,yscale):
     print xs, xp, ys, yp, dp
     return (xp,yp,dp)
 
+def map_to_screen(points,o1,o2,s1,s2):
+    mapped = []
+    od = (float(o2[0]-o1[0]),float(o2[1]-o1[1]))
+    sd = (s2[0]-s1[0],s2[1]-s1[1])
+    print od, sd
+    for p in points:
+        x,y,d=p
+        x = (((x-o1[0])/od[0])*sd[0])+s1[0]
+        y = (((y-o1[1])/od[1])*sd[1])+s1[1]
+        mapped.append((x,y,d))
+    return mapped
+
+        
 def project_points(points,ds,xscale,yscale,X,Y):
     projected = []
     for point in points:
